df7a8b19ae620f2c437596c24e4ac61af005886d
Daniel Washington ignacio <danielwashingtonapp@gmail.com>
2020-10-07 14:25:29 -0300

    menesage

--- src/main.js
+++ src/main.js
@@ -10,13 +10,17 @@ class CustomPlayer{
     }
 
     render(MyPlayerId){
-        let video = document.createElement("video")
 
+        let video = document.createElement("video")
+        document.body.appendChild("video");
+       
         let source = document.createElement("source");
         source.setAttribute("src", "movie.mp4")
+        document.body.appendChild("source");
 
         let text = source = document.createElement("text");
         text.appendChild(document.createTextNode(" Your browser does not support the video tag."))
+        document.body.appendChild("text");
     }
 }
 
